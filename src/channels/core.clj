dead9880bd702f3f234c0d7e8b47bdc2ad2850e1
Tim Ewald <tim.ewald@gmail.com>
2013-07-10 14:51:24 -0400

    Convert router to interceptor instead of part of processor

--- src/channels/core.clj
+++ src/channels/core.clj
@@ -7,81 +7,68 @@
 
 (defn channel? [c] (instance? clojure.core.async.impl.protocols.Channel c))
 
-(defn matched-route
-  [{{path :path :as request} :request :as ctx} router]
-  (when path
-    (if-let [matched-route (some #(when (= path (:path %)) %) router)]
-      matched-route)))
-
 (defn make-processor
-  [interceptors & {router ::router}]
+  [interceptors]
   (fn
     [ctx]
-    (let [matched-route (matched-route ctx router)
-          ctx (assoc ctx :route matched-route)
-          interceptors (concat (:interceptors matched-route) interceptors)]
-      (go
-       (try
-         (loop [[enter leave] (first interceptors)
-                interceptors (rest interceptors)
-                leave-stack nil
-                ctx ctx]
-           (let [res (if enter (enter ctx) ctx)
-                 real-res (if (channel? res)
-                            (<! res)
-                            res)]
-             (if (or (empty? interceptors) (real-res :response))
-               (do ;; have to unwind leave stack here
-                 (println "REVERSING")
-                 (loop [leave (peek leave-stack)
-                        leave-stack (pop leave-stack)
-                        ctx real-res]
-                   (let [res (if leave (leave ctx) ctx)
+    (go
+     (try
+       (loop [[enter leave] (first interceptors)
+              interceptors (rest interceptors)
+              leave-stack nil
+              ctx ctx]
+         (let [res (if enter (enter ctx) ctx)
+               real-res (if (channel? res)
+                          (<! res)
+                          res)]
+           (if (or (empty? interceptors) (real-res :response))
+             (do ;; have to unwind leave stack here
+               (println "REVERSING")
+               (loop [leave (peek leave-stack)
+                      leave-stack (pop leave-stack)
+                      ctx real-res]
+                 (let [res (if leave (leave ctx) ctx)
+                       real-res (if (channel? res)
+                                  (<! res)
+                                  res)]
+                   (if-not (empty? leave-stack)
+                     (recur (peek leave-stack) (pop leave-stack) real-res)
+                     real-res))))
+             (recur (first interceptors) (rest interceptors) (conj leave-stack leave) real-res))))
+       (catch Exception ex ex)))))
+
+(defn processor
+  [ctx]
+  (go
+   (try
+     (loop [ctx ctx]
+       (let [queue (ctx ::queue)
+             [enter leave] (peek queue)
+             stack (ctx ::stack)
+             new-ctx (-> ctx
+                         (assoc ::queue (pop queue))
+                         (assoc ::stack (conj stack leave)))]
+         (let [res (if enter (enter new-ctx) new-ctx)
+               real-res (if (channel? res)
+                          (<! res)
+                          res)]
+           (if (or (empty? queue) (real-res :response))
+             (do ;; have to unwind leave stack here
+               (println "REVERSING")
+               (loop [ctx real-res]
+                 (let [stack (ctx ::stack)
+                       leave (peek stack)
+                       new-ctx (-> ctx
+                                   (assoc ::stack (when-not (empty? stack) (pop stack))))]
+                   (let [res (if leave (leave new-ctx) new-ctx)
                          real-res (if (channel? res)
                                     (<! res)
                                     res)]
-                     (if-not (empty? leave-stack)
-                       (recur (peek leave-stack) (pop leave-stack) real-res)
-                       real-res))))
-               (recur (first interceptors) (rest interceptors) (conj leave-stack leave) real-res))))
-         (catch Exception ex ex))))))
-
-(defn processor
-  [{router ::router :as ctx}]
-  (let [matched-route (matched-route ctx router)
-        ctx (assoc ctx :route matched-route)
-        interceptors (into clojure.lang.PersistentQueue/EMPTY (concat (:interceptors matched-route) (::queue ctx)))
-        ctx (assoc-in ctx [::queue] interceptors)]
-    (go
-     (try
-       (loop [ctx ctx]
-         (let [queue (ctx ::queue)
-               [enter leave] (peek queue)
-               stack (ctx ::stack)
-               new-ctx (-> ctx
-                           (assoc ::queue (pop queue))
-                           (assoc ::stack (conj stack leave)))]
-           (let [res (if enter (enter new-ctx) new-ctx)
-                 real-res (if (channel? res)
-                            (<! res)
-                            res)]
-             (if (or (empty? queue) (real-res :response))
-               (do ;; have to unwind leave stack here
-                 (println "REVERSING")
-                 (loop [ctx real-res]
-                   (let [stack (ctx ::stack)
-                         leave (peek stack)
-                         new-ctx (-> ctx
-                                     (assoc ::stack (when-not (empty? stack) (pop stack))))]
-                     (let [res (if leave (leave new-ctx) new-ctx)
-                           real-res (if (channel? res)
-                                      (<! res)
-                                      res)]
-                       (if (empty? stack)
-                         real-res
-                         (recur real-res))))))
-               (recur real-res)))))
-       (catch Exception ex ex)))))
+                     (if (empty? stack)
+                       real-res
+                       (recur real-res))))))
+             (recur real-res)))))
+     (catch Exception ex ex))))
 
 (defn exception? [e] (instance? Exception e))
 
@@ -144,54 +131,76 @@
 (def double-routes [(fn [ctx] (println "double-route enter") (update-in ctx [:enter] (fnil #(* % 2) 1)))
                     (fn [ctx] (println "double-route leave") (update-in ctx [:leave] (fnil #(* % 2) 1)))])
 
-(def router [{:route-name :double
+(def routes [{:route-name :double
               :path "double"
-              :interceptors [double-routes]}
+              :interceptors [double-routes test-handler]}
              {:route-name :quadruple
               :path "quadruple"
-              :interceptors [double-routes double-routes]}])
+              :interceptors [double-routes double-routes test-handler]}])
+
+(defn matched-route
+  [{{path :path :as request} :request :as ctx} routes]
+  (when path
+    (if-let [matched-route (some #(when (= path (:path %)) %) routes)]
+      matched-route)))
+
+(def test-router-1 [(fn [ctx] 
+                      (let [matched-route (matched-route ctx routes)
+                            ctx (assoc ctx :route matched-route)
+                            route-interceptors (:interceptors matched-route)]
+                        ((make-processor route-interceptors) ctx)))]) ;; adds another go block
+
+(def test-router-2 [(fn [ctx] 
+                      (let [matched-route (matched-route ctx routes)
+                            ctx (assoc ctx :route matched-route)
+                            route-interceptors (:interceptors matched-route)]
+                        (println "MATCHED ROUTE" matched-route)
+                        (println "ROUTE INTERCEPTORS" route-interceptors)
+                        (update-in ctx [::queue]
+                                   (fnil into clojure.lang.PersistentQueue/EMPTY)
+                                   route-interceptors)))]) ;; adds interceptors to existing go block
 
 (def double-request {:request {:path "double"}})
 (def quadruple-request {:request {:path "quadruple"}})
 
 (comment
-  (def proc (make-processor [test-fns test-fns test-handler] ::router router))
+  (def proc (make-processor [test-fns test-fns test-router-1]))
 
   (process-result (proc {:request nil}))
   (process-result (proc {:request {:path "foo"}}))
   (process-result (proc double-request))
   (process-result (proc quadruple-request))
 
-  (def proc (make-processor [test-fns test-fns test-fns-async test-handler] ::router router))
-  (process-result (proc quadruple-request))
+  ;; (def proc (make-processor [test-fns test-fns test-fns-async test-handler] ::router router))
+  ;; (process-result (proc quadruple-request))
 
-  (def proc (make-processor [test-fns test-fns test-fns-async test-fns-except] ::router router))
-  (process-result (proc quadruple-request))
+  ;; (def proc (make-processor [test-fns test-fns test-fns-async test-fns-except] ::router router))
+  ;; (process-result (proc quadruple-request))
 
-  (def proc (make-processor [test-fns test-fns test-fns-async test-fns-events] ::router router))
-  (process-result (proc quadruple-request))
-
-  (process-result
-   (processor
-    {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-handler]) ::router router}))
+  ;; (def proc (make-processor [test-fns test-fns test-fns-async test-fns-events] ::router router))
+  ;; (process-result (proc quadruple-request))
 
   (process-result
    (processor
-    (merge double-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-handler]) ::router router})))
+    {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-router-2])}))
 
   (process-result
    (processor
-    (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-handler]) ::router router})))
+    (merge double-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-router-2])})))
 
-  (process-result
-   (processor
-    (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-handler]) ::router router})))
+  ;; (process-result
+  ;;  (processor
+  ;;   (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-handler]) ::router router})))
 
-  (process-result
-   (processor
-    (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-fns-except]) ::router router})))
+  ;; (process-result
+  ;;  (processor
+  ;;   (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-handler]) ::router router})))
 
-  (process-result
-   (processor
-    (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-fns-events]) ::router router})))
+  ;; (process-result
+  ;;  (processor
+  ;;   (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-fns-except]) ::router router})))
+
+  ;; (process-result
+  ;;  (processor
+  ;;   (merge quadruple-request {::queue (into clojure.lang.PersistentQueue/EMPTY [test-fns test-fns test-fns-async test-fns-events]) ::router router})))
   )
